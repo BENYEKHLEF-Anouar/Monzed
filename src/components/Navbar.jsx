753353a79220bcd22c46ec8faba2912681c343a4
BENYEKHLEF-Anouar <benyekhlef.anouar.solicode@gmail.com>
2025-07-08 17:13:42 +0100

    Initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+function Navbar() {
+    return (
+        <nav className="bg-slate-800 shadow-md sticky top-0 z-50 py-4 px-6 md:px-12 flex justify-between items-center font-poppins">
+            <div className="text-2xl font-extrabold text-primary tracking-tight">
+                Monzed
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
